70d07d4ed1df7193ba072965fd06e1d249cb5db9
Pavel Vasilyev <pavel.vas@mail.ru>
2023-04-14 17:47:35 +0300

    Add checkstyle and fix linter errors

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -10,7 +10,7 @@ import java.util.concurrent.Callable;
 @Command(name = "gendiff", mixinStandardHelpOptions = true, version = "gendiff 1.0",
         description = "Compares two configuration files and shows a difference.")
 
-public class App implements Callable<Integer>{
+public class App implements Callable<Integer> {
     @Option(names = {"-f", "--format"}, paramLabel = "format", description = "output format [default: stylish]")
     private String format;
 
--- app/src/main/java/hexlet/code/Parser.java
+++ app/src/main/java/hexlet/code/Parser.java
@@ -8,6 +8,6 @@ import java.util.TreeMap;
 public class Parser {
     public static TreeMap<String, Object> parse(String content) throws Exception {
         ObjectMapper mapper = new ObjectMapper();
-        return mapper.readValue(content, new TypeReference<>(){});
+        return mapper.readValue(content, new TypeReference<>() { });
     }
 }
