d83719fd53dbd2aa1dd3919173ab134fd14d3941
Pavel Vasilyev <pavel.vas@mail.ru>
2023-04-22 22:30:50 +0300

    Refactor Stylish formatter

--- app/src/main/java/hexlet/code/formatters/Stylish.java
+++ app/src/main/java/hexlet/code/formatters/Stylish.java
@@ -4,17 +4,17 @@ import java.util.List;
 import java.util.Map;
 
 public class Stylish {
-    private static StringBuilder result;
+
     public static String convertResult(List<Map<String, Object>> diffData) {
-        result = new StringBuilder("{\n");
+        StringBuilder result = new StringBuilder("{\n");
         for (Map<String, Object> keyDiff: diffData) {
             switch (keyDiff.get("status").toString()) {
-                case ("added") -> createResultString("  + ", keyDiff, "newValue");
-                case ("removed") -> createResultString("  - ", keyDiff, "oldValue");
+                case ("added") -> result.append(createResultString("  + ", keyDiff, "newValue"));
+                case ("removed") -> result.append(createResultString("  - ", keyDiff, "oldValue"));
                 case ("updated") -> {
-                    createResultString("  - ", keyDiff, "oldValue");
-                    createResultString("  + ", keyDiff, "newValue"); }
-                case ("unchanged") -> createResultString("    ", keyDiff, "oldValue");
+                    result.append(createResultString("  - ", keyDiff, "oldValue"));
+                    result.append(createResultString("  + ", keyDiff, "newValue")); }
+                case ("unchanged") -> result.append(createResultString("    ", keyDiff, "oldValue"));
                 default -> { }
             }
         }
@@ -22,11 +22,7 @@ public class Stylish {
         return result.toString();
     }
 
-    private static void createResultString(String sign, Map<String, Object> keyDiff, String value) {
-        result.append(sign)
-                .append(keyDiff.get("key"))
-                .append(": ")
-                .append(keyDiff.get(value))
-                .append("\n");
+    private static String createResultString(String sign, Map<String, Object> keyDiff, String value) {
+        return sign + keyDiff.get("key") + ": " + keyDiff.get(value) + "\n";
     }
 }
