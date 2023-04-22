1d00c09f1990e934660baa072cd667284c70e9bd
Pavel Vasilyev <pavel.vas@mail.ru>
2023-04-22 20:04:57 +0300

    Refactor Comparator

--- app/src/main/java/hexlet/code/Comparator.java
+++ app/src/main/java/hexlet/code/Comparator.java
@@ -15,21 +15,18 @@ public class Comparator {
         keys.addAll(data2.keySet());
         for (String key: keys) {
             Map<String, Object> keyDiff = new LinkedHashMap<>();
+            keyDiff.put("key", key);
             if (!data1.containsKey(key) && data2.containsKey(key)) {
-                keyDiff.put("key", key);
                 keyDiff.put("status", "added");
                 keyDiff.put("newValue", data2.get(key));
             } else if (data1.containsKey(key) && !data2.containsKey(key)) {
-                keyDiff.put("key", key);
                 keyDiff.put("status", "removed");
                 keyDiff.put("oldValue", data1.get(key));
             } else if (!Objects.equals(data1.get(key), data2.get(key))) {
-                keyDiff.put("key", key);
                 keyDiff.put("status", "updated");
                 keyDiff.put("oldValue", data1.get(key));
                 keyDiff.put("newValue", data2.get(key));
             } else {
-                keyDiff.put("key", key);
                 keyDiff.put("status", "unchanged");
                 keyDiff.put("oldValue", data1.get(key));
             }
